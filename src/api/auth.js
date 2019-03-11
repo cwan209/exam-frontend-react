8d1c46585ce97796fbbd38d224be934ec8d2d9cb
lukechenluwang@gmail.com <luke.w@bmyg.com.au>
2019-03-11 18:31:43 +1100

    added loading for buttons

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,6 @@ import * as config from '../config';
 
 export const signup = (email, password) => {
   const url = `${config.apiHost}v1/signup`;
-  console.log(url);
 
   return fetch(url,
     {
@@ -15,20 +14,9 @@ export const signup = (email, password) => {
         password: password
       })
     })
+    .then(response => response.json())
     .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Something went wrong on api server!');
-      }
-    })
-    .then(response => {
-      console.debug(response);
-      return {
-        success: true,
-        payload: response
-      }
-      // ...
+      return response;
     }).catch(error => {
       console.error(error);
       return {
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
 import Input from '@material-ui/core/Input';
@@ -13,8 +13,108 @@ import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Checkbox from '@material-ui/core/Checkbox';
-import { signup } from "../api/auth";
+import green from '@material-ui/core/colors/green';
+import {signup} from "../api/auth";
+
+class LoginPage extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    showPassword: false,
+    loading: false,
+  };
+
+  handleChange = prop => event => {
+    console.log("handleChange", prop);
+    this.setState({[prop]: event.target.value});
+  };
+
+  handleClickShowPassword = () => {
+    this.setState(state => ({showPassword: !state.showPassword}));
+  };
+
+  handleSubmit = event => {
+    const {email, password} = this.state;
+    event.preventDefault();
+    this.setState({loading: true});
+
+    signup(email, password).then(
+      response => {
+        this.setState({loading: false});
+        if (response.success) {
+          console.log('success', response.user);
+        } else {
+          console.log('fail', response.error);
+        }
+      }
+    );
+
+  };
+
+  render() {
+    const {classes} = this.props;
+    const {loading} = this.state;
+
+    return (
+      <form className={classes.container} noValidate autoComplete="off">
+        <Paper className={classes.paper}>
+          <TextField
+            id="standard-name"
+            label="Email"
+            className={classes.textField}
+            value={this.state.email}
+            onChange={this.handleChange('email')}
+            margin="normal"
+          />
+          <FormControl className={classNames(classes.margin, classes.textField)}>
+            <InputLabel htmlFor="adornment-password">Password</InputLabel>
+            <Input
+              id="adornment-password"
+              type={this.state.showPassword ? 'text' : 'password'}
+              value={this.state.password}
+              onChange={this.handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="Toggle password visibility"
+                    onClick={this.handleClickShowPassword}
+                  >
+                    {this.state.showPassword ? <Visibility/> : <VisibilityOff/>}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary"/>}
+            label="Remember me"
+          />
+
+          <div className={classes.wrapper}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={this.handleSubmit}
+              disabled={loading}
+            >
+              Sign in
+            </Button>
+            {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+          </div>
+        </Paper>
+
+      </form>
+    )
+  }
+}
+
+LoginPage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 const styles = theme => ({
   container: {
@@ -46,97 +146,21 @@ const styles = theme => ({
   menu: {
     width: 200,
   },
+  buttonProgress: {
+    color: '#3f51b5',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12,
+  },
+  wrapper: {
+    margin: theme.spacing.unit,
+    position: 'relative',
+  },
 });
 
-class LoginPage extends React.Component {
-  state = {
-    email: '',
-    password: '',
-    showPassword: false,
-  };
-
-  handleChange = prop => event => {
-    console.log("handleChange", prop);
-    this.setState({ [prop]: event.target.value });
-  };
-
-  handleClickShowPassword = () => {
-    this.setState(state => ({ showPassword: !state.showPassword }));
-  };
-
-  handleSubmit = event => {
-    const {email, password} = this.state;
-    event.preventDefault();
-
-    signup(email, password).then(
-      result => {
-        if (result.success) {
-          alert(result.response);
-        }
-
-      }
-
-    );
-
-  };
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <form className={classes.container} noValidate autoComplete="off">
-        <Paper className={classes.paper}>
-          <TextField
-            id="standard-name"
-            label="Email"
-            className={classes.textField}
-            value={this.state.email}
-            onChange={this.handleChange('email')}
-            margin="normal"
-          />
-          <FormControl className={classNames(classes.margin, classes.textField)}>
-            <InputLabel htmlFor="adornment-password">Password</InputLabel>
-            <Input
-              id="adornment-password"
-              type={this.state.showPassword ? 'text' : 'password'}
-              value={this.state.password}
-              onChange={this.handleChange('password')}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="Toggle password visibility"
-                    onClick={this.handleClickShowPassword}
-                  >
-                    {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={this.handleSubmit}
-            className={classes.submit}
-          >
-            Sign in
-          </Button>
-        </Paper>
-
-      </form>
-    )
-  }
-}
-
-LoginPage.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(LoginPage);
+
+
